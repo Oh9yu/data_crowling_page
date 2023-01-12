56e679c38c352ef7b8c1808d14c9b636172a6150
Oh9yu <kog6148@gmail.com>
2023-01-12 16:16:48 +0900

    초기세팅

---
+++ datacrowling/src/page/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Content from './content/Content';
+import Tab from './tab/Tab';
+
+const Main = () => {
+  return (
+    <div>
+      main search
+      <Tab />
+      <Content />
+    </div>
+  );
+};
+
+export default Main;
